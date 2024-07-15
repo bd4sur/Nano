f8bf066bdf4f3a0d49f6236ac50594f43c1406ea
BD4SUR <20069428+bd4sur@users.noreply.github.com>
2024-07-16 00:44:43 +0800

    2024-07-16

--- nlp/model.py
+++ nlp/model.py
@@ -1,13 +1,6 @@
 """
 Forked from https://github.com/karpathy/nanoGPT
 BD4SUR 2023.12
-
-Full definition of a GPT Language Model, all of it in this single file.
-References:
-1) the official GPT-2 TensorFlow implementation released by OpenAI:
-https://github.com/openai/gpt-2/blob/master/src/model.py
-2) huggingface/transformers PyTorch implementation:
-https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
 """
 
 import math
