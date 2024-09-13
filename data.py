4c4d1b272db71f56a1b59e4e82a0139d6a642ce2
BD4SUR <20069428+bd4sur@users.noreply.github.com>
2024-09-13 23:05:15 +0800

    2024-09-13

--- data.py
+++ data.py
@@ -6,20 +6,21 @@ from tqdm import tqdm
 from tokenizer import Tokenizer
 
 def generate_nlg_dataset(input_file, data_dir="dataset", is_build_tokenizer=True, block_size=512, overlap_ratio=0.5):
-    print(f"Reading raw text file...")
     input_file_path = os.path.join(os.path.dirname(__file__), data_dir, input_file)
-    with open(input_file_path, "r", encoding="utf-8") as f:
-        fulltext = f.read()
-    print(f"  Length of dataset in characters: {len(fulltext):,}")
-
     tokenizer_path = os.path.join(os.path.dirname(__file__), data_dir, 'tokenizer.json')
+
     tokenizer = Tokenizer()
     if is_build_tokenizer:
-        print(f"Building tokenizer from raw text...")
-        tokenizer.build_from_text(fulltext, tokenizer_path)
+        print(f"Building tokenizer from raw text file...")
+        tokenizer.build_from_file(input_file_path, tokenizer_path)
     else:
         print(f"Loading tokenizer...")
         tokenizer.load_from_config(tokenizer_path)
+
+    print(f"Reading raw text file...")
+    with open(input_file_path, "r", encoding="utf-8") as f:
+        fulltext = f.read()
+    print(f"  Length of dataset in characters: {len(fulltext):,}")
 
     print(f"Slicing raw text into blocks...")
     text_slices = []
@@ -30,19 +31,27 @@ def generate_nlg_dataset(input_file, data_dir="dataset", is_build_tokenizer=True
             text_slices.append(tslice) # 每一条数据都比block_size多一个，用于预测下一字符的训练
         i += int(block_size * overlap_ratio)
 
-    print(f"Shuffling and encoding text blocks...")
+    del fulltext
+
+    print(f"Encoding text blocks...")
+    for i in tqdm(range(len(text_slices))):
+        text_slices[i] = tokenizer.encode(text_slices[i])
+
+    print(f"Shuffling text blocks...")
     train_ids = []
     val_ids = []
     line_indexes = list(range(len(text_slices)))
     random.shuffle(line_indexes)
     for li in tqdm(range(0, int(len(text_slices) * 0.9))):
-        train_ids.append(tokenizer.encode(text_slices[line_indexes[li]]))
+        train_ids.append(text_slices[line_indexes[li]])
     for li in tqdm(range(int(len(text_slices) * 0.9), len(text_slices))):
-        val_ids.append(tokenizer.encode(text_slices[line_indexes[li]]))
+        val_ids.append(text_slices[line_indexes[li]])
 
+    print(f"Cast to numpy array...")
     train_ids = np.array(train_ids, dtype=np.uint16)
     val_ids = np.array(val_ids, dtype=np.uint16)
 
+    print(f"Saving pickel file...")
     dataset = {
         "train_ids": train_ids,
         "val_ids": val_ids
@@ -53,7 +62,7 @@ def generate_nlg_dataset(input_file, data_dir="dataset", is_build_tokenizer=True
     print(f"Done.")
 
 def main():
-    generate_nlg_dataset("psycho.txt", data_dir="dataset", block_size=512, overlap_ratio=0.1)
+    generate_nlg_dataset("input.txt", data_dir="dataset", block_size=512, overlap_ratio=0.5)
 
 if __name__ == "__main__":
     main()
--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,6 @@
 import json
+import functools
+from tqdm import tqdm
 
 class Tokenizer:
     def __init__(self):
@@ -7,10 +9,12 @@ class Tokenizer:
         self.vocab_size = 0
 
     # encoder: take a string, output a list of integers
+    @functools.lru_cache
     def encode(self, text):
         return [(self.stoi[c] if (c in self.stoi) else (self.vocab_size - 1)) for c in text]
 
     # decoder: take a list of integers, output a string
+    @functools.lru_cache
     def decode(self, code_list):
         return ''.join([self.itos[i] for i in code_list])
 
@@ -30,6 +34,33 @@ class Tokenizer:
         # create a mapping from characters to integers
         self.stoi = { ch:i for i,ch in enumerate(chars) }
         self.itos = { i:ch for i,ch in enumerate(chars) }
+        config = {
+            "vocab_size": self.vocab_size,
+            "stoi": self.stoi,
+            "itos": self.itos
+        }
+        with open(config_path, 'w') as f:
+            json.dump(config, f)
+
+    def build_from_file(self, text_path, config_path):
+        def read_chunk(filepath, chunk_size=65536):
+            with open(filepath, mode="r", encoding="utf-8") as f:
+                while True:
+                    chunk = f.read(chunk_size)
+                    if not chunk:
+                        return
+                    yield chunk
+        vocab = set({})
+        text_iterator = read_chunk(text_path, chunk_size=16777216)
+        for chunk in tqdm(text_iterator):
+            vocab = vocab.union(set(chunk))
+        vocab = list(vocab)
+        vocab.append("<|undefined|>")
+        self.vocab_size = len(vocab)
+        print(f"  Vocab size: {self.vocab_size:,}")
+        # create a mapping from characters to integers
+        self.stoi = { ch:i for i,ch in enumerate(vocab) }
+        self.itos = { i:ch for i,ch in enumerate(vocab) }
         config = {
             "vocab_size": self.vocab_size,
             "stoi": self.stoi,
