7511b8987285461243e78d86de973cdcbedc92a1
BD4SUR <20069428+bd4sur@users.noreply.github.com>
2026-05-02 22:33:47 +0800

    2026-05-02

--- infer/celestial.c
+++ infer/celestial.c
@@ -2607,7 +2607,7 @@ void render_sky(Nano_GFX *gfx,
             }
         }
         else {
-            #pragma omp parallel for schedule(dynamic)
+            #pragma omp parallel for schedule(static)
             for (int32_t y = y1; y < y2; y += _downsampling_factor) {
                 for (int32_t x = x1; x < x2; x += _downsampling_factor) {
                     // 观察者到该像素的方向向量（从屏幕坐标系转回地平天球的笛卡尔坐标系）
